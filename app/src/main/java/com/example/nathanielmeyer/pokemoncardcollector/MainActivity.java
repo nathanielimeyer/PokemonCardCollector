aaf40b3edcd57ae9b71bf7da1bca27c80279b199
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-05-26 15:11:55 -0700

    Add toast to search button

--- app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/MainActivity.java
+++ app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.nathanielmeyer.pokemoncardcollector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.searchByNameButton) Button mSearchByNameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mSearchByNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Searching...", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
