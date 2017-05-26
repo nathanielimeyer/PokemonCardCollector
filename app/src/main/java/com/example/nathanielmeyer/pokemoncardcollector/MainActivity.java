8f3e5114d98bce31d359f2d7b0e9a91f30387cdc
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-05-26 16:33:32 -0700

    Add EditText, pass to intent, and log

--- app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/MainActivity.java
+++ app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/MainActivity.java
@@ -3,15 +3,19 @@ package com.example.nathanielmeyer.pokemoncardcollector;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.searchByNameButton) Button mSearchByNameButton;
+    @Bind(R.id.searchText) EditText mSearchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Searching...", Toast.LENGTH_LONG).show();
+                String searchString = mSearchText.getText().toString();
+                Log.d(TAG, searchString);
                 Intent intent = new Intent(MainActivity.this, SearchResults.class);
+                intent.putExtra("searchString", searchString);
                 startActivity(intent);
             }
         });
