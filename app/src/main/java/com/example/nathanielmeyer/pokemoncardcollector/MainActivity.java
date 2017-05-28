095180deb5c90e2ae8a966ac0d7e942e561c82c8
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-05-28 12:57:19 -0700

    Add custom typeface

--- app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/MainActivity.java
+++ app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.nathanielmeyer.pokemoncardcollector;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
@@ -14,12 +16,17 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.searchByNameButton) Button mSearchByNameButton;
     @Bind(R.id.searchText) EditText mSearchText;
+    @Bind(R.id.textView) TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Typeface amatic = Typeface.createFromAsset(getAssets(), "fonts/AmaticSC-Regular.ttf");
+        mTextView.setTypeface(amatic);
+
         mSearchByNameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
