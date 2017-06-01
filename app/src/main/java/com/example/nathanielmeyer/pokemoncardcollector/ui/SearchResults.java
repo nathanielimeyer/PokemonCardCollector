983d01d638171b67acd0ac6be4c4a1bccf535504
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-06-01 16:08:12 -0700

    Display card names from parsed JSON

--- app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/ui/SearchResults.java
+++ app/src/main/java/com/example/nathanielmeyer/pokemoncardcollector/ui/SearchResults.java
@@ -29,10 +29,6 @@ public class SearchResults extends AppCompatActivity {
 
     @Bind(R.id.resultsTextView) TextView mResultsTextView;
 
-    private String[] results = new String[] {"Pikachu", "Bulbasaur",
-            "Flutterby", "Meowth", "Ekans", "Clefairy",
-            "Clefable", "Charizard", "Squirtle", "Goldeen",
-            "Lickitung", "Snorlax", "Dugduo", "Spearow", "Ratatat"};
     @Bind(R.id.listView) ListView mListView;
 
     public ArrayList<Card> mCards = new ArrayList<>();
@@ -44,23 +40,11 @@ public class SearchResults extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, results);
-        mListView.setAdapter(adapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String result = ((TextView)view).getText().toString();
-                Toast.makeText(SearchResults.this, result, Toast.LENGTH_LONG).show();
-
-            }
-        });
-
         Intent intent = getIntent();
         String searchString = intent.getStringExtra("searchString");
+
         mResultsTextView.setText("Results for " + searchString + ":");
         getCardsByName(searchString);
-
     }
 
     private void getCardsByName(String name) {
@@ -73,16 +57,41 @@ public class SearchResults extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    String jsonData = response.body().string();
-                    if (response.isSuccessful()) {
-                        Log.v(TAG, jsonData);
-                        mCards = PokemonTCGService.processResults(response);
+                mCards = PokemonTCGService.processResults(response);
+
+                SearchResults.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String[] cardNames = new String[mCards.size()];
+                        for (int i = 0; i < cardNames.length; i++) {
+                            cardNames[i] = mCards.get(i).getName();
+                        }
+
+                        ArrayAdapter adapter = new ArrayAdapter(SearchResults.this, android.R.layout.simple_list_item_1, cardNames);
+                        mListView.setAdapter(adapter);
+
+//                        for (Card card : mCards) {
+//                            Log.d(TAG, "Id: " + card.getId());
+//                            Log.d(TAG, "Name: " + card.getName());
+//                            Log.d(TAG, "ImageUrl: " + card.getImageUrl());
+//                            Log.d(TAG, "ImageUrlHiRes: " + card.getmImageUrlHiRes());
+//                            Log.d(TAG, "Subtype: " + card.getSubtype());
+//                            Log.d(TAG, "Supertype: " + card.getSupertype());
+//                            Log.d(TAG, "Hp: " + card.getHp());
+//                            Log.d(TAG, "Retreat Cost: " + card.getRetreatCost());
+//                            Log.d(TAG, "Number: " + card.getNumber());
+//                            Log.d(TAG, "Artist: " + card.getArtist());
+//                            Log.d(TAG, "Series: " + card.getSeries());
+//                            Log.d(TAG, "Set: " + card.getSet());
+//                            Log.d(TAG, "SetCode: " + card.getSetCode());
+//                            Log.d(TAG, "Types: " + card.getTypes());
+//                            Log.d(TAG, "Attacks: " + card.getAttacks());
+//                            Log.d(TAG, "Weaknesses: " + card.getWeaknesses());
+//                        }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                });
             }
+
         });
     }
 }
