c424b47296e5c13674e5a171a64874b26b7f9e3f
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-06-10 15:37:08 -0700

    Final changes to save cards

--- app/src/main/java/com/nathanielimeyer/pokemoncardcollector/Constants.java
+++ app/src/main/java/com/nathanielimeyer/pokemoncardcollector/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final String POKEMONTCG_NAME_QUERY_PARAMETER = "name";
     public static final String PREFERENCES_QUERY_KEY = "query";
     public static final String FIREBASE_CHILD_SEARCHED_POKEMON = "searchedPokemon";
+    public static final String FIREBASE_CHILD_CARDS = "cards";
+
 }
