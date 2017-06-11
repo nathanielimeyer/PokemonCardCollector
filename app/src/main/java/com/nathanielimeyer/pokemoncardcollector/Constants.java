b033223343168e8d04ebd05902e0162bcfd5cb13
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-06-10 17:46:34 -0700

    Remove commented example

--- app/src/main/java/com/nathanielimeyer/pokemoncardcollector/Constants.java
+++ app/src/main/java/com/nathanielimeyer/pokemoncardcollector/Constants.java
@@ -1,14 +1,9 @@
 package com.nathanielimeyer.pokemoncardcollector;
 
 public class Constants {
-//    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
-//    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
-//    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
-//    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
     public static final String POKEMONTCG_CARDS_BASE_URL = "https://api.pokemontcg.io/v1/cards";
     public static final String POKEMONTCG_NAME_QUERY_PARAMETER = "name";
     public static final String PREFERENCES_QUERY_KEY = "query";
     public static final String FIREBASE_CHILD_SEARCHED_POKEMON = "searchedPokemon";
     public static final String FIREBASE_CHILD_CARDS = "cards";
-
 }
